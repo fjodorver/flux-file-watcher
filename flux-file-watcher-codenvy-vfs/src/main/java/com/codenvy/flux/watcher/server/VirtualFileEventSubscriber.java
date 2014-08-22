1b0a2d5ebaf777ace44401bf12f486618b30a6f9
Stéphane Tournié <stephane.tournie@serli.com>
2014-08-22 16:51:59 +0200

    fixing rename & move resource + refactoring method onEvent

--- flux-file-watcher-codenvy-vfs/src/main/java/com/codenvy/flux/watcher/server/VirtualFileEventSubscriber.java
+++ flux-file-watcher-codenvy-vfs/src/main/java/com/codenvy/flux/watcher/server/VirtualFileEventSubscriber.java
@@ -26,6 +26,8 @@ import com.codenvy.api.project.server.FolderEntry;
 import com.codenvy.api.project.server.ProjectManager;
 import com.codenvy.api.project.server.VirtualFileEntry;
 import com.codenvy.api.vfs.server.VirtualFile;
+import com.codenvy.api.vfs.server.observation.MoveEvent;
+import com.codenvy.api.vfs.server.observation.RenameEvent;
 import com.codenvy.api.vfs.server.observation.VirtualFileEvent;
 import com.codenvy.flux.watcher.core.Repository;
 import com.codenvy.flux.watcher.core.RepositoryEvent;
@@ -55,73 +57,89 @@ public class VirtualFileEventSubscriber implements EventSubscriber<VirtualFileEv
 
     @Override
     public void onEvent(VirtualFileEvent event) {
-        final String eventWorkspace = event.getWorkspaceId();
-        final String eventPath = event.getPath();
-        LOG.debug("new event in workspace " + eventWorkspace + " concerning >" + eventPath + "<");
-        FolderEntry workspaceBaseFolder = null;
-        try {
-            workspaceBaseFolder = projectManager.getProjectsRoot(eventWorkspace);
-        } catch (ServerException e) {
-            LOG.error("Couldn't get projects", e);
-        }
+        VirtualFileEvent.ChangeType eventType = event.getType();
+        String eventWorkspace = event.getWorkspaceId();
+        LOG.debug("new event in workspace " + eventWorkspace);
 
-        Set<Project> projects = repository.getSynchronizedProjects();
-        Project project = FluentIterable.from(projects)
-                                        .firstMatch(new Predicate<Project>() {
-                                            @Override
-                                            public boolean apply(Project project) {
-                                                return eventPath.startsWith(project.path());
-                                            }
-                                        })
-                                        .orNull();
+        // TODO workspace should not be hardcoded
+        if (eventWorkspace.equals("1q2w3e")) {
 
-        String resourceProjectPath = eventPath.substring(project.path().length());
-        if (resourceProjectPath.startsWith("/")) {
-            resourceProjectPath = resourceProjectPath.substring(1);
-        }
+            RepositoryEventType repositoryEventType = null;
+            Resource resource;
 
-        if (workspaceBaseFolder != null && project != null) {
-            VirtualFileEvent.ChangeType eventType = event.getType();
-            RepositoryEventType repoEventType = null;
-            if ((eventType == VirtualFileEvent.ChangeType.CREATED) || (eventType == VirtualFileEvent.ChangeType.MOVED)
-                || (eventType == VirtualFileEvent.ChangeType.RENAMED)) {
-                repoEventType = RepositoryEventType.PROJECT_RESOURCE_CREATED;
-            } else if (eventType == VirtualFileEvent.ChangeType.CONTENT_UPDATED) {
-                repoEventType = RepositoryEventType.PROJECT_RESOURCE_MODIFIED;
-            } else if (eventType == VirtualFileEvent.ChangeType.DELETED) {
-                repoEventType = RepositoryEventType.PROJECT_RESOURCE_DELETED;
-            }
+            final String eventPath = event.getPath();
+            LOG.debug("eventPath : >" + eventPath + "<");
 
-            RepositoryEvent repoEvent = null;
-            if (repoEventType != null) {
-                try {
-                    VirtualFileEntry eventPathVFEntry = workspaceBaseFolder.getChild(resourceProjectPath);
-                    Resource resource = null;
-                    if (eventPathVFEntry != null) {
-                        VirtualFile eventPathVFile = eventPathVFEntry.getVirtualFile();
-                        long lastModificationDate = eventPathVFile.getLastModificationDate();
+            Set<Project> projects = repository.getSynchronizedProjects();
+            Project project = FluentIterable.from(projects)
+                                            .firstMatch(new Predicate<Project>() {
+                                                @Override
+                                                public boolean apply(Project project) {
+                                                    return eventPath.startsWith(project.path());
+                                                }
+                                            })
+                                            .orNull();
 
-                        if (eventPathVFEntry.isFile() && !event.isFolder()) {
-                            byte[] content = IOUtils.toByteArray(eventPathVFile.getContent().getStream());
-                            resource = Resource.newFile(resourceProjectPath, lastModificationDate, content);
-                        } else if (eventPathVFEntry.isFolder() && event.isFolder()) {
-                            resource = Resource.newFolder(resourceProjectPath, lastModificationDate);
-                        }
-                    } else {
-                        // case of file/folder deletion event
-                        if (!event.isFolder()) {
-                            resource = Resource.newFile(resourceProjectPath, System.currentTimeMillis(), new byte[0]);
-                        } else if (event.isFolder()) {
-                            resource = Resource.newFolder(resourceProjectPath, System.currentTimeMillis());
-                        }
+            if (project != null) {
+                String projectPath = project.path();
+
+                if (eventPath.startsWith(projectPath)) {
+                    String resourceRelativePath = eventPath.substring(projectPath.length());
+                    if (eventType == VirtualFileEvent.ChangeType.CONTENT_UPDATED) {
+                        repositoryEventType = RepositoryEventType.PROJECT_RESOURCE_MODIFIED;
+                    } else if (eventType == VirtualFileEvent.ChangeType.CREATED) {
+                        repositoryEventType = RepositoryEventType.PROJECT_RESOURCE_CREATED;
+                    } else if (eventType == VirtualFileEvent.ChangeType.DELETED) {
+                        repositoryEventType = RepositoryEventType.PROJECT_RESOURCE_DELETED;
+                    } else if (eventType == VirtualFileEvent.ChangeType.MOVED) {
+                        repositoryEventType = RepositoryEventType.PROJECT_RESOURCE_CREATED;
+                    } else if (eventType == VirtualFileEvent.ChangeType.RENAMED) {
+                        repositoryEventType = RepositoryEventType.PROJECT_RESOURCE_CREATED;
                     }
-                    repoEvent = new RepositoryEvent(repoEventType, resource, project);
-                } catch (ServerException | ForbiddenException | IOException e) {
-                    LOG.error("Couldn't get child named " + eventPath + " in " + workspaceBaseFolder.getPath(), e);
+
+                    // getting concret resource data from VFS
+                    try {
+                        FolderEntry workspaceRootFolder = projectManager.getProjectsRoot(eventWorkspace);
+                        VirtualFileEntry vfEntry = workspaceRootFolder.getChild(resourceRelativePath);
+
+                        VirtualFile vFile = (vfEntry != null ? vfEntry.getVirtualFile() : null);
+                        if (event.isFolder()) {
+                            resource = Resource.newFolder(resourceRelativePath,
+                                                          (vFile != null ? vFile.getLastModificationDate() : System.currentTimeMillis()));
+                            LOG.debug("newFolder : " + resourceRelativePath);
+                        } else {
+                            byte[] content = (vFile != null ? IOUtils.toByteArray(vFile.getContent().getStream()) : new byte[0]);
+                            resource = Resource.newFile(resourceRelativePath,
+                                                        (vFile != null ? vFile.getLastModificationDate() : System.currentTimeMillis()), content);
+                            LOG.debug("newFile : " + resourceRelativePath + ", containing "+(vFile != null ? IOUtils.toString(vFile.getContent().getStream(), "UTF-8") : "nothing"));
+                        }
+                        // and firing event to Flux clients
+                        if (repositoryEventType != null) {
+                            repositoryEventBus.fireRepositoryEvent(new RepositoryEvent(repositoryEventType, resource, project));
+                        }
+
+                    } catch (ServerException | ForbiddenException | IOException e) {
+                        LOG.error("Couldn't get projects", e);
+                    }
+                }
+
+                String eventOldPath = null;
+                // rename and move are treated as create and delete
+                if (eventType == VirtualFileEvent.ChangeType.MOVED) {
+                    eventOldPath = ((MoveEvent)event).getOldPath();
+                } else if (eventType == VirtualFileEvent.ChangeType.RENAMED) {
+                    eventOldPath = ((RenameEvent)event).getOldPath();
+                }
+                if (eventOldPath != null && eventOldPath.startsWith(projectPath)) {
+                    Resource oldResource;
+                    if (event.isFolder()) {
+                        oldResource = Resource.newFolder(eventOldPath.substring(projectPath.length()), System.currentTimeMillis());
+                    } else {
+                        oldResource = Resource.newFile(eventOldPath.substring(projectPath.length()), System.currentTimeMillis(), new byte[0]);
+                    }
+                    repositoryEventBus.fireRepositoryEvent(new RepositoryEvent(RepositoryEventType.PROJECT_RESOURCE_DELETED, oldResource, project));
                 }
             }
-
-            repositoryEventBus.fireRepositoryEvent(repoEvent);
         }
     }
 }
