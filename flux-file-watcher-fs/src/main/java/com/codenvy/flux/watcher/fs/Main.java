4020da630cc3f76129be9dcd46e8b584f22185bb
Fjodor Vershinin <fjodor@vershinin.net>
2016-07-26 22:34:04 +0300

    Removed flux-file-watcher-codenvy-vfs module. Add resource consumption test

--- flux-file-watcher-fs/src/main/java/com/codenvy/flux/watcher/fs/Main.java
+++ flux-file-watcher-fs/src/main/java/com/codenvy/flux/watcher/fs/Main.java
@@ -10,24 +10,58 @@
  *******************************************************************************/
 package com.codenvy.flux.watcher.fs;
 
-import com.codenvy.flux.watcher.core.Credentials;
-import com.codenvy.flux.watcher.core.Repository;
-import com.codenvy.flux.watcher.core.RepositoryModule;
-import com.google.inject.Guice;
-import com.google.inject.Injector;
+import com.codenvy.flux.watcher.core.Resource;
+import com.codenvy.flux.watcher.core.spi.Project;
+import com.google.common.jimfs.Jimfs;
 
-import java.net.MalformedURLException;
-import java.net.URL;
+import javax.security.auth.login.Configuration;
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryMXBean;
+import java.nio.file.FileSystem;
+import java.text.MessageFormat;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import static java.nio.file.Files.createDirectory;
+import static org.mockito.Mockito.mock;
 
 /**
  * @author Kevin Pollet
  */
 public class Main {
-    public static void main(String[] args) throws MalformedURLException {
-        final Injector injector = Guice.createInjector(new RepositoryModule(), new JDKProjectModule());
+    public static void main(String[] args) throws IOException {
+        /*final Injector injector = Guice.createInjector(new RepositoryModule(), new JDKProjectModule());
 
         final Repository repository = injector.getInstance(Repository.class);
         repository.addRemote(new URL("http://localhost:3000"), Credentials.DEFAULT_USER_CREDENTIALS);
-        repository.addProject("flux", "/Users/kevin/Desktop/flux/flux");
+        repository.addProject("flux", "/Users/kevin/Desktop/flux/flux");*/
+        final String PROJECT_ID = "codenvy-project-id";
+        final String PROJECT_PATH = "/codenvy-project";
+
+        MemoryMXBean mxBean = ManagementFactory.getMemoryMXBean();
+        FileSystem fileSystem = Jimfs.newFileSystem();
+        createDirectory(fileSystem.getPath(PROJECT_PATH));
+        createDirectory(fileSystem.getPath(PROJECT_PATH).resolve("src"));
+        JDKProjectWatchService watchService = mock(JDKProjectWatchService.class);
+        Project project = new JDKProject(fileSystem, watchService, PROJECT_ID, PROJECT_PATH);
+
+        byte[] data = new byte[1000000];
+        Random random = new Random();
+        random.nextBytes(data);
+
+        List<Long> results = new ArrayList<>();
+
+        for (int i = 0; i < 100; i++) {
+            String filename = MessageFormat.format("src/{0}.txt", i);
+            Resource resource = Resource.newFile(filename, LocalDateTime.now().getNano(), data);
+            project.createResource(resource);
+            project.updateResource(resource);
+            project.deleteResource(resource);
+            results.add(mxBean.getHeapMemoryUsage().getUsed());
+        }
+        System.out.println(results.stream().mapToLong(x -> x).average().orElse(0));
     }
 }
