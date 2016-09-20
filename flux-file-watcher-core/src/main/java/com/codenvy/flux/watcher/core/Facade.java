08e62c3601d5a72f68e1a000445c23d57ece6341
Fjodor Vershinin <fjodor@vershinin.net>
2016-09-20 10:05:54 +0300

    Created facade

---
+++ flux-file-watcher-core/src/main/java/com/codenvy/flux/watcher/core/Facade.java
@@ -0,0 +1,72 @@
+package com.codenvy.flux.watcher.core;
+
+import com.codenvy.flux.watcher.core.enums.EventType;
+import com.codenvy.flux.watcher.core.event.ProjectEvent;
+import com.codenvy.flux.watcher.core.event.ResourceEvent;
+import com.codenvy.flux.watcher.core.model.Project;
+import com.codenvy.flux.watcher.core.service.ConnectionService;
+import com.codenvy.flux.watcher.core.service.ProjectService;
+import com.google.common.base.Preconditions;
+import com.google.common.eventbus.EventBus;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import javax.inject.Inject;
+import java.net.URI;
+
+public class Facade {
+
+    @Inject
+    private ProjectService projectService;
+
+    @Inject
+    private ConnectionService connectionService;
+
+    @Inject
+    private EventBus eventBus;
+
+    @Inject
+    public void init() {
+        connectionService.registerEvent("getProjectRequest", new ProjectEvent(EventType.REQUEST));
+        connectionService.registerEvent("getProjectResponse", new ProjectEvent(EventType.RESPONSE));
+        connectionService.registerEvent("getResourceRequest", new ResourceEvent(EventType.REQUEST));
+        connectionService.registerEvent("getResourceResponse", new ResourceEvent(EventType.RESPONSE));
+        connectionService.registerEvent("resourceCreated", new ResourceEvent(EventType.CREATE));
+        connectionService.registerEvent("resourceChanged", new ResourceEvent(EventType.CHANGE));
+        connectionService.registerEvent("resourceDeleted", new ResourceEvent(EventType.DELETE));
+        connectionService.registerEvent("resourceStored", new ResourceEvent(EventType.STORE));
+    }
+
+    public void addRemote(URI uri, Credentials credentials){
+        Preconditions.checkNotNull(uri);
+        Preconditions.checkNotNull(credentials);
+        connectionService.addRemote(uri, credentials);
+    }
+
+    public void removeRemote(URI uri){
+        Preconditions.checkNotNull(uri);
+        connectionService.removeRemote(uri);
+    }
+
+    public void connectToChannel(URI uri, String channel){
+        Preconditions.checkNotNull(uri);
+        Preconditions.checkNotNull(channel);
+        connectionService.connectToChannel(uri, channel);
+    }
+
+    public void disconnectFromChannel(URI uri, String channel){
+        throw new NotImplementedException();
+    }
+
+    public void connectProject(String name, String path){
+        Preconditions.checkNotNull(name);
+        Preconditions.checkNotNull(path);
+        projectService.connect(new Project().setName(name).setPath(path));
+    }
+
+    public void disconnectProject(String name){
+        Preconditions.checkNotNull(name);
+        Project project = projectService.find(name);
+        Preconditions.checkNotNull(project);
+        projectService.disconnect(project);
+    }
+}
--- flux-file-watcher-fs/src/main/java/com/codenvy/flux/watcher/fs/Main.java
+++ flux-file-watcher-fs/src/main/java/com/codenvy/flux/watcher/fs/Main.java
@@ -11,26 +11,13 @@
 package com.codenvy.flux.watcher.fs;
 
 import com.codenvy.flux.watcher.core.Credentials;
+import com.codenvy.flux.watcher.core.Facade;
 import com.codenvy.flux.watcher.core.RepositoryModule;
-import com.codenvy.flux.watcher.core.enums.EventType;
-import com.codenvy.flux.watcher.core.event.ProjectEvent;
-import com.codenvy.flux.watcher.core.event.ResourceEvent;
-import com.codenvy.flux.watcher.core.handler.ProjectHandler;
-import com.codenvy.flux.watcher.core.handler.ResourceHandler;
-import com.codenvy.flux.watcher.core.model.Project;
-import com.codenvy.flux.watcher.core.service.ConnectionService;
-import com.codenvy.flux.watcher.core.service.ProjectService;
-import com.codenvy.flux.watcher.fs.service.WatcherServiceImpl;
-import com.google.common.collect.Sets;
-import com.google.common.eventbus.EventBus;
-import com.google.common.util.concurrent.Service;
-import com.google.common.util.concurrent.ServiceManager;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.Set;
 
 /**
  * @author Kevin Pollet
@@ -38,38 +25,11 @@ import java.util.Set;
 public class Main {
     public static void main(String[] args) throws URISyntaxException {
         Injector injector = Guice.createInjector(new RepositoryModule(), new JDKModule());
-
-        ProjectService projectService = injector.getInstance(ProjectService.class);
-        ConnectionService connectionService = injector.getInstance(ConnectionService.class);
-        ProjectHandler projectHandler = injector.getInstance(ProjectHandler.class);
-        ResourceHandler resourceHandler = injector.getInstance(ResourceHandler.class);
-        WatcherServiceImpl watcherService = injector.getInstance(WatcherServiceImpl.class);
-        EventBus eventBus = injector.getInstance(EventBus.class);
-        eventBus.register(projectHandler);
-        eventBus.register(resourceHandler);
-
-        Set<Service> services = Sets.newHashSet();
-        services.add(watcherService);
-        ServiceManager serviceManager = new ServiceManager(services);
-        serviceManager.startAsync().awaitHealthy();
+        Facade facade = injector.getInstance(Facade.class);
 
         URI uri = new URI("http://localhost:3000");
-
-        connectionService.addRemote(uri, new Credentials("defaultuser"));
-        connectionService.registerEvent("getProjectRequest", new ProjectEvent(EventType.REQUEST));
-        connectionService.registerEvent("getProjectResponse", new ProjectEvent(EventType.RESPONSE));
-        connectionService.registerEvent("getResourceRequest", new ResourceEvent(EventType.REQUEST));
-        connectionService.registerEvent("getResourceResponse", new ResourceEvent(EventType.RESPONSE));
-        connectionService.registerEvent("resourceCreated", new ResourceEvent(EventType.CREATE));
-        connectionService.registerEvent("resourceChanged", new ResourceEvent(EventType.CHANGE));
-        connectionService.registerEvent("resourceDeleted", new ResourceEvent(EventType.DELETE));
-        connectionService.registerEvent("resourceStored", new ResourceEvent(EventType.STORE));
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        connectionService.connectToChannel(uri, "defaultuser");
-        projectService.connect(new Project().setName("flux").setPath("/home/fjodor/Documents/"));
+        facade.addRemote(uri, new Credentials("defaultuser"));
+        facade.connectToChannel(uri, "defaultuser");
+        facade.connectProject("flux", "/home/fjodor/Doccuments/");
     }
 }
